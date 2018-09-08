c30fc4417636466c36dc55d45bc9199a2887bd6d
Patric Khiev <pskhiev@gmail.com>
2018-09-08 13:59:08 -0500

    app1 completed

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      display: '',
+    }
+  }
+
+  showDisplay = (event) => {
+    this.setState({display:event.target.value})
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(event) => this.showDisplay(event)} placeholder="Enter Text Here" />
+        <p>{this.state.display}</p>
       </div>
     );
   }
